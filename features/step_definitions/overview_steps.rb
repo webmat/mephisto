f15b34b327f66c1ca9087c0c5d6dfcc9ec56a974
Eric Kidd <git@randomhacks.net>
2008-12-28 16:26:58 -0500

    Replace helper methods with step invocations
    Thanks to brynary on #webrat for pointing me towards this feature!
    There's no need to create helper functions to share code between steps--
    instead, we can call out to other steps from within steps.
    
    With a few more tweaks, I could write about 80% of my steps directly in
    the *.features language, and reserve Ruby for those cases when I need to
    poke around in the e-mail delivery queue, or something similarly exotic.
    
    The only remaining drawback with this code is the handling of nested
    quote characters.  I can probably clean this up, perhaps using a helper
    method and some single quotes here and there.

--- features/step_definitions/overview_steps.rb
+++ features/step_definitions/overview_steps.rb
@@ -1,18 +1,8 @@
-class Cucumber::Rails::World
-  def visit_overview_page
-    visit '/admin'
-  end
-
-  def should_see_overview_page
-    response.body.should =~ /Create new article/m
-    response.body.should =~ /Recent activity/m
-  end
-end
-
 Given /I try to access the overview page/ do
-  visit_overview_page
+  visit '/admin'
 end
 
 Then /I should see the overview page/ do
-  should_see_overview_page
+  Then "I should see \"Create new article\""
+  And "I should see \"Recent activity\""
 end
